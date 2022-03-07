a742d93d420f70cabcaf95c97f31d47b18e7c56e
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-03-07 18:42:21 +0800

    Added Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <ColorModeProvider options={{ useSystemColorMode: true }}>
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
+  )
 }
 
-export default MyApp
+export default MyApp;
