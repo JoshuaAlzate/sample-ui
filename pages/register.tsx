29249586d4aa35b03f66399669dc1db58a7f6741
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-03-10 13:27:45 +0800

    Added Button Submit

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,4 +1,4 @@
-import { FormControl, FormLabel, Input, FormHelperText, Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import { Formik } from "formik";
 import InputField from "../components/input-field";
 
@@ -6,10 +6,11 @@ const Register = () => {
     return (
         <Box mt={8} mx={'auto'} maxW={800} w={'100%'}>
             <Formik initialValues={{ username: '', password: '' }} onSubmit={(values) => console.log(values)}>
-                {() => (
+                {({ isSubmitting }) => (
                     <form>
                         <InputField name="username" label="Username"/>
                         <InputField name="password" label="Password" type="password" />
+                        <Button mt={4} isLoading={isSubmitting} type="submit" colorScheme="teal">Register</Button>
                     </form>
                 )}
             </Formik>
