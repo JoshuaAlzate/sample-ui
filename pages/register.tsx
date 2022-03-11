939d28e860e93e1c4aaa0e2703a98c2b09dd4baf
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-03-11 15:35:18 +0800

    Change HTML form to Formik Form Component

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,17 +1,17 @@
 import { Box, Button } from "@chakra-ui/react";
-import { Formik } from "formik";
+import { Form, Formik } from "formik";
 import InputField from "../components/input-field";
 
 const Register = () => {
     return (
         <Box mt={8} mx={'auto'} maxW={800} w={'100%'}>
-            <Formik initialValues={{ username: '', password: '' }} onSubmit={(values) => console.log(values)}>
+            <Formik initialValues={{ username: '', password: '' }} onSubmit={(values) => { console.log(values) }}>
                 {({ isSubmitting }) => (
-                    <form>
-                        <InputField name="username" label="Username"/>
+                    <Form>
+                        <InputField name="username" label="Username" />
                         <InputField name="password" label="Password" type="password" />
                         <Button mt={4} isLoading={isSubmitting} type="submit" colorScheme="teal">Register</Button>
-                    </form>
+                    </Form>
                 )}
             </Formik>
         </Box>
