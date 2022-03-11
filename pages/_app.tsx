51e00ee23584b0512d601ca541d6d5f70a5417ee
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-03-11 18:53:14 +0800

    Initialised GraphQL Client Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { createClient, Provider } from 'urql'
+
+const client = createClient({
+  url: 'http://127.0.0.1:8080/graphql',
+  fetchOptions: {
+    credentials: 'include'
+  }
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <ColorModeProvider options={{ useSystemColorMode: true }}>
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <Provider value={client}>
+      <ChakraProvider>
+        <ColorModeProvider options={{ useSystemColorMode: true }}>
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </Provider>
   )
 }
 
