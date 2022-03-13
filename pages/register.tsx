bb3beaf14146707498197735a5f04172a531e0c7
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-03-13 23:34:58 +0800

    Formatted Register Page Code

--- pages/register.tsx
+++ pages/register.tsx
@@ -6,13 +6,13 @@ import { useRegisterMutation } from "../graphql/generated/graphql";
 import toErrorMap from "../utils/errorMap";
 
 const Register = () => {
-  const router = useRouter();
+    const router = useRouter();
     const [, register] = useRegisterMutation();
     return (
         <Box mt={8} mx={'auto'} maxW={800} w={'100%'}>
             <Formik initialValues={{ username: '', password: '' }} onSubmit={async (values, { setErrors }) => {
                 const response = await register(values);
-                if(response.data?.register.errors) setErrors(toErrorMap(response.data.register.errors));
+                if (response.data?.register.errors) setErrors(toErrorMap(response.data.register.errors));
                 else if (response.data?.register.user) router.push('/');
             }}>
                 {({ isSubmitting }) => (
