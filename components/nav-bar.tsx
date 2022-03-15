caa053085e512196d8292c2d17a462e0379aff01
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-03-15 20:45:15 +0800

    Updated Navigation Bar

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,12 +1,34 @@
-import { Box, Button, Flex } from "@chakra-ui/react";
+import { Box, Link, Flex, Text, Button } from "@chakra-ui/react";
+import NextLink from 'next/link';
+import { useMeQuery } from "../graphql/generated/graphql";
 
 const NavBar = () => {
+    const [{ data, fetching }] = useMeQuery();
+    let navigationBody = null;
+    if (data?.me.user) {
+        navigationBody = (
+            <>
+                <Link verticalAlign={'middle'}>{data.me.user?.username}</Link>
+                <Button>Logout</Button>
+            </>
+        );
+    } else {
+        navigationBody = (
+            <>
+                <NextLink href={'/login'}>
+                    <Link mr={4}>Login</Link>
+                </NextLink>
+                <NextLink href={'/register'}>
+                    <Link>Register</Link>
+                </NextLink>
+            </>
+        );
+    }
     return (
         <>
             <Flex borderBottom='1px' borderColor='gray.200'>
                 <Box p={5} ml={'auto'}>
-                    <Button>Login</Button>
-                    <Button>Register</Button>
+                    {navigationBody}
                 </Box>
             </Flex>
         </>
