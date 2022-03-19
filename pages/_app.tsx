f5bae8d0ba6a577d86966835e4fb536ec3c6ddff
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-03-20 01:23:43 +0800

    Added handling for the cache update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,31 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-import { createClient, Provider } from 'urql'
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql'
+import { cacheExchange } from '@urql/exchange-graphcache'
+import { LoginMutation, LogoutMutation, MeDocument, MeQuery, RegisterMutation } from '../graphql/generated/graphql'
+import { betterQueryUpdate } from '../utils/betterUpdateQuery'
 
 const client = createClient({
   url: 'http://127.0.0.1:8080/graphql',
   fetchOptions: {
     credentials: 'include'
-  }
+  },
+  exchanges: [dedupExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        login: (result, args, cache, info) => {
+          betterQueryUpdate<LoginMutation, MeQuery>(cache, { query: MeDocument }, result, (data, query) => data.login.errors ? query : { me: data.login });
+        },
+        register: (result, args, cache, info) => {
+          betterQueryUpdate<RegisterMutation, MeQuery>(cache, { query: MeDocument }, result, (data, query) => data.register.errors ? query : { me: data.register });
+        },
+        logout: (result, args, cache, info) => {
+          betterQueryUpdate<LogoutMutation, MeQuery>(cache, { query: MeDocument }, result, () => ({ me: null }));
+        }
+      }
+    }
+  }), fetchExchange]
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
