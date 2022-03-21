481d59041d47f13ffbc716379a31f65970b1afac
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-03-21 23:57:31 +0800

    Remove initialisation of Client Side Graphql Client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,42 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-import { createClient, dedupExchange, fetchExchange, Provider } from 'urql'
-import { cacheExchange } from '@urql/exchange-graphcache'
-import { LoginMutation, LogoutMutation, MeDocument, MeQuery, RegisterMutation } from '../graphql/generated/graphql'
-import { betterQueryUpdate } from '../utils/betterUpdateQuery'
-
-const client = createClient({
-  url: 'http://127.0.0.1:8080/graphql',
-  fetchOptions: {
-    credentials: 'include'
-  },
-  exchanges: [dedupExchange, cacheExchange({
-    updates: {
-      Mutation: {
-        login: (result, args, cache, info) => {
-          betterQueryUpdate<LoginMutation, MeQuery>(cache, { query: MeDocument }, result, (data, query) => data.login.errors ? query : { me: data.login });
-        },
-        register: (result, args, cache, info) => {
-          betterQueryUpdate<RegisterMutation, MeQuery>(cache, { query: MeDocument }, result, (data, query) => data.register.errors ? query : { me: data.register });
-        },
-        logout: (result, args, cache, info) => {
-          betterQueryUpdate<LogoutMutation, MeQuery>(cache, { query: MeDocument }, result, () => ({ me: null }));
-        }
-      }
-    }
-  }), fetchExchange]
-})
+import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider value={client}>
       <ChakraProvider>
         <ColorModeProvider options={{ useSystemColorMode: true }}>
           <Component {...pageProps} />
         </ColorModeProvider>
       </ChakraProvider>
-    </Provider>
   )
 }
 
