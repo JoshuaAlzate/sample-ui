4a010533ad0ce62f8dcb24c4100bcdb0f4bc73d6
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-04-08 00:41:00 +0800

    Added email field at the register page

--- pages/register.tsx
+++ pages/register.tsx
@@ -12,7 +12,7 @@ const Register = () => {
     const [, register] = useRegisterMutation();
     return (
         <Box mt={8} mx={'auto'} maxW={800} w={'100%'}>
-            <Formik initialValues={{ username: '', password: '' }} onSubmit={async (values, { setErrors }) => {
+            <Formik initialValues={{ username: '', password: '', email: '' }} onSubmit={async (values, { setErrors }) => {
                 const response = await register(values);
                 if (response.data?.register.errors) setErrors(toErrorMap(response.data.register.errors));
                 else if (response.data?.register.user) router.push('/');
@@ -20,6 +20,7 @@ const Register = () => {
                 {({ isSubmitting }) => (
                     <Form>
                         <InputField name="username" label="Username" />
+                        <InputField name="email" label="Email" type="email" />
                         <InputField name="password" label="Password" type="password" />
                         <Flex mt={4} justifyContent={'space-between'}>
                             <Button isLoading={isSubmitting} type="submit" colorScheme="teal">Register</Button>
